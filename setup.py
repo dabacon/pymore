a4a5a77806ed5c65d22b882bb99390c2a2f04321
Dave Bacon <dabacon@gmail.com>
2021-02-13 14:12:06 -0800

    Add setup file and version

---
+++ setup.py
@@ -0,0 +1,49 @@
+# Copyright 2021 The pymore Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+import os
+from setuptools import setup
+
+name = 'pymore'
+
+description = 'Mo python, less problems. Helpful python utilities.'
+
+long_description = io.open('README.md', encoding='utf-8').read()
+
+# Read in requirements
+requirements = open('requirements.txt').readlines()
+requirements = [r.strip() for r in requirements]
+dev_requirements = open('requirements-dev.txt').readlines()
+dev_requirements = [r.strip() for r in dev_requirements]
+
+pymore_packages = ['pymore']
+
+setup(
+    name=name,
+    version="0.1",
+    url='http://github.com/dabacon/pymore',
+    author='The pymore Developers',
+    author_email='dabacon@gmail.com',
+    python_requires=('>=3.6.0'),
+    install_requires=requirements,
+    extras_require={
+        'dev_env': dev_requirements,
+    },
+    license='Apache 2',
+    description=description,
+    long_description=long_description,
+    packages=pymore_packages,
+    include_package_data=True,
+)
