29651d8002f86903ab4981819262218a0e9d8fbc
Dave Bacon <dabacon@google.com>
2025-08-15 13:03:52 -0700

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ pymore_packages = ["pymore"]
 
 setup(
     name=name,
-    version="0.1.3",
+    version="0.1.4",
     url="http://github.com/dabacon/pymore",
     author="The pymore Developers",
     author_email="dabacon@gmail.com",
